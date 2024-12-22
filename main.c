6b7915e142dc8dae3dd3ffe41f4204cc68011b45
Omar Zeyad <o.zeyad@outlook.com>
2024-12-22 13:54:40 -0500

    pushing main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h> // Include the standard input-output header
+
+int main() {
+    // Print "Hello, World!" to the console
+    printf("Hello, World!\n");
+    return 0; // Return 0 to indicate successful execution
+}
